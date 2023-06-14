5d8f8d7294959882619f0d75116ee481ee6b8b66
Oliinyk Yevgenii <oliinykyevgenii@gmail.com>
2023-06-14 18:08:03 +0300

    cv update

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -156,3 +156,5 @@ const ContactsForm = () => {
 export default ContactsForm;
 
 
+// const emailValidationPattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    // const phoneNumberPatternUkrain = /^((\+?3)?8)?((0\(\d{2}\)?)|(\(0\d{2}\))|(0\d{2}))\d{7}$/;
